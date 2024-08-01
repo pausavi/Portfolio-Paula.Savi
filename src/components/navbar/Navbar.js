f494d5408d5e5ab3cf0719bbed16c156749ce74a
Paula Savi <paulamsavi@gmail.com>
2024-08-01 16:43:56 -0300

    first commit

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as S from "./Navbar.styles";
+
+const Navbar = () => {
+  return ( <S.ContainerNavbar>
+    dentro del container 
+
+ </S.ContainerNavbar>)
+   
+  
+};
+
+export default Navbar;
