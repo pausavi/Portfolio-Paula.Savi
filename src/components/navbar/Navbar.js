ba5b511331eec173747522974857e64335121840
Paula Savi <paulamsavi@gmail.com>
2024-08-05 00:15:49 -0300

    add burger menu mobile and desktop

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from "./Navbar.styles";
 
 const Navbar = () => {
+
+    const [showMenuBurger, setShowMenuBurger]= useState(false);
   return ( <S.ContainerNavbar>
-    dentro del container 
+    <S.ContainerLogo>
+        <img src='/img/LogoPPS.png' alt='Logo Paula Savi'/>
+        <div>
+            <h1>Paula Savi</h1>
+            <h2>Frontend Developer</h2>
+        </div>
+    </S.ContainerLogo>
+    <S.ContainerBurger onClick={() => setShowMenuBurger(!showMenuBurger)}>
+        <S.BurgerMenu/>
+        <S.BurgerMenu/>
+        <S.BurgerMenu/>
+    </S.ContainerBurger>
+    <S.ContainerItems showMenuBurger={showMenuBurger}>
+        <p>About me</p>
+        <p>Projects</p>
+        <S.Button>Contact me</S.Button>
+    </S.ContainerItems>
+    
 
  </S.ContainerNavbar>)
    
--- src/components/navbar/Navbar.styles.js
+++ src/components/navbar/Navbar.styles.js
@@ -2,6 +2,93 @@ import styled from "styled-components";
 import { colors } from "../../styles/Colors";
 
 export const ContainerNavbar = styled.div`
-height: 100px;
-background-color: ${colors.TEXT_GREY_NEUTRAL};
+max-width: 200vw;
+height: 150px;
+box-shadow: ${colors.BOX_SHADOW};
+display: flex;
+align-items: center;
+justify-content: space-between;
+padding: 0 20px;
+background-color: ${colors.TEXT_WHITE};
+`
+
+export const ContainerLogo = styled.div`
+    display: flex;
+    align-items: center;
+
+    h1, h2 {
+    font-weight: 700;
+    color: ${colors.TEXT_BLACK};
+    margin: 0;
+    }
+    h1{
+    font-size: 2rem;
+    }
+    h2{
+    background: linear-gradient(275deg, rgba(47,71,151,1) 47%, rgba(65,90,119,1) 79%);
+    -webkit-background-clip: text; 
+    color: transparent;
+    margin: 0;
+    }
+
+    img{
+    width: 120px;
+    padding-right: 20px;
+    }`;
+
+export const ContainerItems = styled.div`
+    display: flex;
+    align-items: center;
+
+    p{
+    cursor:pointer;
+    padding: 0 13px;
+    text decoration: none;
+    color: ${colors.TEXT_BLACK};
+    font-weight: 500 }
+
+  @media(max-width: 768px){
+    display:  ${(props) =>(props.showMenuBurger ? 'inline-grid': 'none')};
+    position: absolute;
+    top: 150px;
+    background: ${colors.TEXT_WHITE};
+    box-shadow: ${colors.BOX_SHADOW};
+    padding: 10px 0 20px 0;
+    width: 100%;
+    left: 0;
+
+    a{
+    padding: 10px 20px;
+    };
+  }
 `;
+export const Button = styled.button`
+    color: ${colors.TEXT_WHITE};
+    box-shadow:${colors.BOX_SHADOW_BUTTON};
+    background: linear-gradient(275deg, rgba(47,71,151,1) 47%, rgba(65,90,119,1) 79%);
+    padding: 10px 10px;
+    border-radius: 99px;
+    border:none;
+    margin: 10px 0;
+    font size: 16px;
+    font-weight: 700;
+    cursor: pointer;
+
+    @media(max-width: 768px){
+    width: 40%;
+    margin: 5px 10px 0 10px;}
+`;
+
+export const ContainerBurger = styled.div`
+    display: none;
+    @media(max-width: 768px){
+    display: block;
+    }
+`;
+
+export const BurgerMenu= styled.div`
+    width: 35px;
+    height: 2px;
+    background-color: ${colors.TEXT_BLACK};
+    margin: 6px 0;
+`
